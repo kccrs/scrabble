7511a24cafb4ed8ec5bf8c991c825649359cf9ed
Casey Cross <hellocaseycross@gmail.com>
2016-09-19 22:49:05 -0600

    Initial file set up

---
+++ test/index.js
@@ -0,0 +1,8 @@
+const assert = require('chai').assert;
+require('../lib/components/Application.js');
+
+describe('The test bundle', function() {
+  it('should be connected', function() {
+    assert(true);
+  });
+});
